2d79907831fd6fa227ac41ee73fe91aeec01d0c7
evanryan210 <evanryan99@gmail.com>
2022-08-29 23:58:29 -0400

    Late night minor changes and analysis

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import * as styles from './styles';
+import * as styles from './styles'
 
 export const NavBar = () =>{
     return(
       <header className = {styles.header}>
         <div className = {styles.logo}>OrangeSpark</div>
         <div className = {styles.navBarButtons}>
-          <a href="#">Services</a>
-          <a href="#">Our Work</a>
           <a href="#">Contact Us</a>
+          <a href="#">Our Solution</a>
+          <a href="#">Past Projects</a>
+          <a href="#">Meet Us</a>
         </div>
       </header>
           )
