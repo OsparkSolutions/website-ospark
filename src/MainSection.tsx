8b877d0bb1ac9292f494fda2c7c613d2d962b2fb
evanryan210 <evanryan99@gmail.com>
2022-08-30 00:00:51 -0400

    8/30 late night minor edits

--- src/MainSection.tsx
+++ src/MainSection.tsx
@@ -3,8 +3,13 @@ import * as styles from './styles';
 
 export const MainSection = () =>{
     return(
-        <div>
-            Main Section
+        <div className = {styles.mainSection}>
+            <div>
+                <p>Orange Spark Solutions, LLC is a technology solutions provider. We work with Startups, Small & Mid-sized businesses on specialized projects or as a full-service technology partner. We work closely with you to provide effiecient, scalable, right-sized technology solutions at reasonable costs.</p>
+            </div>
+            <div>
+                <p>Got tech problems. We have the solutions.</p>
+            </div>
         </div>
     )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -5,18 +5,28 @@ export const fieldwork = mergeStyles({
     fontFamily: 'fieldwork, sans-serif',
 })
 
+export const logo = mergeStyles([fieldwork], {fontSize: '50px', color: '#f37322'});
+
 export const navBarButtons = mergeStyles({
     display: 'flex',
     /** .navBar a{} **/
-    justifyContent: 'right',
-    'a':[fieldwork, {textDecoration: 'none', fontSize: '20px', color: 'pink', marginLeft: '20px', marginRight: '20px'}],
-    
-});
-export const logo = mergeStyles([fieldwork], {fontSize: '50px', color: '#f37322'});
-export const header = mergeStyles({
-    display: 'flex',
-    padding: '20px',
     justifyContent: 'space-between',
+    'a':[fieldwork, {textDecoration: 'none', fontSize: '20px', color: 'pink', marginLeft: '20px', marginRight: '20px'}],
     alignItems: 'center',
 });
 
+export const header = mergeStyles({
+    display: 'flex',
+    padding: '20px',
+    alignItems: 'center',
+    // why shouldnt this be here and not in navBarButtons:
+    //justifyContent: 'space-between',
+});
+
+export const mainSection = mergeStyles({
+    display: 'flex',
+    justifyContent: 'center',
+    //why cant i center a flex column
+    //flexDirection: 'column',
+    'p':[{justifyContent: 'center',}]
+})
