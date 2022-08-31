94745cf4005a3d1bd9ba994efbe1ae66435dbfa6
Evan <evanryan99@gmail.com>
2022-08-31 10:48:47 -0400

    changed 'space-between' in navbar to correct spot

--- src/styles.ts
+++ src/styles.ts
@@ -10,7 +10,7 @@ export const logo = mergeStyles([fieldwork], {fontSize: '50px', color: '#f37322'
 export const navBarButtons = mergeStyles({
     display: 'flex',
     /** .navBar a{} **/
-    justifyContent: 'space-between',
+    
     'a':[fieldwork, {textDecoration: 'none', fontSize: '20px', color: 'pink', marginLeft: '20px', marginRight: '20px'}],
     alignItems: 'center',
 });
@@ -20,13 +20,17 @@ export const header = mergeStyles({
     padding: '20px',
     alignItems: 'center',
     // why shouldnt this be here and not in navBarButtons:
-    //justifyContent: 'space-between',
+    justifyContent: 'space-between',
 });
 
 export const mainSection = mergeStyles({
+    paddingTop: '100px',
     display: 'flex',
+    alignContent: 'center',
     justifyContent: 'center',
-    //why cant i center a flex column
-    //flexDirection: 'column',
-    'p':[{justifyContent: 'center',}]
+    flexDirection: 'column',
+    'p':[{justifyContent: 'center', textAlign: 'center'}],
+    
+    
+    
 })
