15a8c49e4fed025a426cbd800e6104623e9d9821
Evan <evanryan99@gmail.com>
2022-08-31 10:48:13 -0400

    compass work 8/31 AM

--- src/MainSection.tsx
+++ src/MainSection.tsx
@@ -4,12 +4,8 @@ import * as styles from './styles';
 export const MainSection = () =>{
     return(
         <div className = {styles.mainSection}>
-            <div>
-                <p>Orange Spark Solutions, LLC is a technology solutions provider. We work with Startups, Small & Mid-sized businesses on specialized projects or as a full-service technology partner. We work closely with you to provide effiecient, scalable, right-sized technology solutions at reasonable costs.</p>
-            </div>
-            <div>
-                <p>Got tech problems. We have the solutions.</p>
-            </div>
+            <p>Orange Spark Solutions, LLC is a technology solutions provider. We work with Startups, Small & Mid-sized businesses on specialized projects or as a full-service technology partner. We work closely with you to provide effiecient, scalable, right-sized technology solutions at reasonable costs.</p>
+            <p>Got tech problems. We have the solutions.</p>
         </div>
     )
 }
